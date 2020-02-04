8641822274cf5fec0c1e74f60a8b6ae95d56832b
EdwardossLu <50994768+EdwardossLu@users.noreply.github.com>
2020-02-04 18:25:50 +1100

    Added comments for clarity

--- WaypointSystem.cs
+++ WaypointSystem.cs
@@ -15,14 +15,16 @@ public class PathSystem : MonoBehaviour
     [Tooltip("List of waypoints for the object to move towards.")]
     [SerializeField] private List<Transform> waypoints = new List<Transform>();
 
-    private int _point = 0;
+    // The amount of waypoints the player has reached.
+    private int _points = 0;
 
+    // The combine wait time for "waitingTimer" and global time.
     private float _timer = 0;
     
 
     private void Awake() 
     {
-        if (_point != 0) _point = 0;
+        if (_points != 0) _points = 0;
     }
 
     private void Update() 
@@ -30,43 +32,51 @@ public class PathSystem : MonoBehaviour
         MoveToNextWaypoint();
     }
 
+    // Move the gameObject to the desired location.
     private void MoveToNextWaypoint()
     {
         float waypointMoveSpeed = speed * Time.deltaTime;
 
-        if (_point != waypoints.Count && Time.time >= _timer)
+        // Check if the points don't match the amount of waypoints && the stop timer doesn't match the timer. 
+        if (_points != waypoints.Count && Time.time >= _timer)
         {
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[_point].position, waypointMoveSpeed);
+            // Move the gameObject to the disired location.
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[_points].position, waypointMoveSpeed);
 
-            if (transform.position == waypoints[_point].position)
+            // If the gameObject has reached this location of the waypoint.
+            if (transform.position == waypoints[_points].position)
             {
-                ++_point;
-                _timer = Time.time + waitingTimer;
+                ++_points;                                                                                  // Plus 1 to "_points".
+                _timer = Time.time + waitingTimer;                                       // plus current game time to "waitingTimer". 
             }
         }
-        else if (_point == waypoints.Count && Time.time >= _timer)
+        // Check if the points does match the amount of waypoints && the stop timer doesn't match the timer. 
+        else if (_points == waypoints.Count && Time.time >= _timer)
         {
+            // Move the gameObject to the first waypoint.
             transform.position = Vector3.MoveTowards(transform.position, waypoints[0].position, waypointMoveSpeed);
 
+            // If the gameObject has reached the first waypoint.
             if (transform.position == waypoints[0].position)
             {
-                _point = 0;
-                _timer = Time.time + waitingTimer;
+                _points = 0;                                                                                // Reset "_points".
+                _points = Time.time + waitingTimer;                                      // plus current game time to "waitingTimer". 
             }
         }
     }
 
+    // Display each waypoint inside the editor.
     private void OnDrawGizmos() 
     {
         if (waypoints.Count > 0)
         {
+            // Display all waypoint sphere as blue.
             Gizmos.color = Color.blue;
-
             foreach (Transform T in waypoints)
                 Gizmos.DrawSphere(T.position, .1f);
 
+            // Display all directional lines are white.
             Gizmos.color = Color.white;
-
             for (int i = 0; i < waypoints.Count - 1; i++)
             {
                 Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
